f8dfb1f2e6a8eab7f455202d041421c475343f63
Los_Emilos <los.emilos.email@gmail.com>
2022-11-21 02:02:12 +0100

    change create_task and logging

--- jira_methods.py
+++ jira_methods.py
@@ -1,9 +1,9 @@
 from jira import JIRA
 
-jira = JIRA(server='https://testblachemi.atlassian.net', basic_auth=('blachemi@proton.me', 'PaAlcPTf5rDDE1DbaQ7hCFC7'))
+jira = JIRA(server='https://testblachemi.atlassian.net', basic_auth=('blachemi@proton.me', 'iXAnYrrGv7ZZt5Vlp2QaE981'))
 
 
-def create_task(sumarydb, componentnamedb):
-    new_issue = jira.create_issue(project='PI', summary=sumarydb, customfield_10063=componentnamedb,
+def create_task(summarydb, componentnamedb):
+    new_issue = jira.create_issue(project='PI', summary=summarydb, customfield_10063=componentnamedb,
                                   description='look', issuetype={'name': '[System] Incident'})
     return new_issue.key
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import db_statement
 import jira_methods
 import time
+import logging
+from datetime import datetime
 
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
 errors = (db_statement.geterrors())
 for x in errors:
     id_db = str(x[0])
     component = x[2]
     error_name = x[3]
-    print(x)
+    logging.info(datetime.now().strftime("%d_%m_%Y_:%H:%M:%S") + ' ' + str(x) + '\n')
     id_jira = jira_methods.create_task(id_db+' '+component+' '+error_name, component)
     db_statement.seterrors(id_db, id_jira)
-    time.sleep(15)
+    time.sleep(2)
